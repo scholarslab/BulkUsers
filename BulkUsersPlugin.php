3c08fde3681f129f9746da90c0b39cbcbc0365f9
Jeremy Boggs <jeremy@clioweb.org>
2013-08-26 10:43:05 -0400

    Adds ACL, so only super users can bulk add users.

--- BulkUsersPlugin.php
+++ BulkUsersPlugin.php
@@ -7,10 +7,27 @@ if (!defined('BULKUSERS_PLUGIN_DIR')) {
 class BulkUsersPlugin extends Omeka_Plugin_AbstractPlugin {
 
     // Hooks and Filters
+    protected $_hooks = array(
+        'define_acl'
+    );
+
     protected $_filters = array(
         'admin_navigation_main'
     );
 
+    /**
+     *  define_acl hook
+     */
+    public function hookDefineAcl($args)
+    {
+        $acl = $args['acl'];
+
+        $acl->addResource('BulkUsers_Index');
+
+        // Specifically deny Admin users.
+        $acl->deny('admin', 'BulkUsers_Index');
+
+    }
 
     /**
      * Adds a button to the admin's main navigation.
@@ -23,8 +40,10 @@ class BulkUsersPlugin extends Omeka_Plugin_AbstractPlugin {
 
         $nav[] = array(
             'label' => __('Bulk Users'),
-            'uri' => url('bulk-users')
+            'uri' => url('bulk-users'),
+            'resource' => 'BulkUsers_Index'
         );
+
         return $nav;
 
     }
